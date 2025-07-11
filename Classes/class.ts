18daf27156ce53450e5e01d25840588181b58bfc
SwapniL1563 <choratswapnil15@gmail.com>
2025-07-12 00:13:06 +0530

    typescript basic

--- Classes/class.ts
+++ Classes/class.ts
@@ -1,13 +1,22 @@
 class User {
-    name:string
+    public name:string
     email:string
-    readonly city:string = ""
+    private readonly city:string = ""
     constructor(name:string,email:string){
         this.name = name;
         this.email = email;
+        this.city
     }
 }
 
 // creating an object
 const swapnil = new User("swap","swap@gmail.com");
 
+// better way
+class User1 {
+    constructor(
+    public name:string,
+    public email:string){
+        
+    }
+}
